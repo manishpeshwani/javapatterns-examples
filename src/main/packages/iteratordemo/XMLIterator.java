f86756d90fb0cb0a872fae740100e1b41641aeed
manishpeshwani <manishpeshwani@yahoo.com>
2017-01-28 18:31:28 +0530

    - Added attribute to the root element to demonstrate attribute handling - Added javadoc comments in XMLItertor.

--- src/main/packages/iteratordemo/XMLIterator.java
+++ src/main/packages/iteratordemo/XMLIterator.java
@@ -11,132 +11,146 @@ import javax.xml.stream.events.Attribute;
 import javax.xml.stream.events.Characters;
 import javax.xml.stream.events.EndElement;
 
-public class XMLIterator implements Iterator<String>{
-	
+public class XMLIterator implements Iterator<String> {
+
 	private XMLEventReader reader;
 	private String rootElementLocalName;
 	private String documentElementLocalName;
 	private static final String START_TAG = "<";
 	private static final String END_TAG = ">";
 
-	public XMLIterator(XMLEventReader reader, String rootElementLocalName, String documentElementLocalName){
+	/**
+	 * 
+	 * @param reader - XMLEventReader instance which will read the events
+	 * @param rootElementLocalName - The root element name of the fragment to be read by the itarator.
+	 * @param documentElementLocalName - The root element of the XML document
+	 */
+	public XMLIterator(XMLEventReader reader, String rootElementLocalName, String documentElementLocalName) {
 		this.reader = reader;
 		this.rootElementLocalName = rootElementLocalName;
 		this.documentElementLocalName = documentElementLocalName;
 	}
-	
+
 	@Override
 	public boolean hasNext() {
 		try {
 			XMLEvent event = reader.peek();
-			
-			
-			while(event !=null && event.getEventType() == XMLStreamConstants.CHARACTERS 
-					&& (event.asCharacters().getData().equals("\n\t") || event.asCharacters().getData().equals("\n") || event.asCharacters().getData().equals("\t"))){
+
+			//If the event is a new line character skip it and peek the next element. Keep skipping till the next event is a new line character.
+			while (event != null && event.getEventType() == XMLStreamConstants.CHARACTERS
+					&& (event.asCharacters().getData().equals("\n\t") || event.asCharacters().getData().equals("\n")
+							|| event.asCharacters().getData().equals("\t"))) {
 				reader.nextEvent();
 				event = reader.peek();
 			}
-			
-			if(event == null || (event.getEventType() == XMLStreamConstants.END_ELEMENT 
-					&& event.asEndElement().getName().getLocalPart().equals(documentElementLocalName))){
+
+			//If the last element is the root element of the document, then return false, else return true.
+			if (event == null || (event.getEventType() == XMLStreamConstants.END_ELEMENT
+					&& event.asEndElement().getName().getLocalPart().equals(documentElementLocalName))) {
 				return false;
-			}else {
+			} else {
 				return true;
 			}
 		} catch (XMLStreamException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return false;
-		
+
 	}
 
 	@Override
 	public String next() {
-		StringBuilder nextElementBuilder = null; 
+		StringBuilder nextElementBuilder = null;
 		try {
-			while(reader.hasNext()){
+			
+			while (reader.hasNext()) {
 				
 				XMLEvent event = reader.nextEvent();
-				if(event.getEventType() == XMLStreamConstants.START_ELEMENT){
+				if (event.getEventType() == XMLStreamConstants.START_ELEMENT) {
 					StartElement startElement = event.asStartElement();
-                    String qName = startElement.getName().getLocalPart();
-                    
-                    if(qName.equals(rootElementLocalName)){
-                    	nextElementBuilder = new StringBuilder();
-                    	handleStartElement(startElement, nextElementBuilder);
-                    }else {
-                    	handleStartElement(startElement, nextElementBuilder);
-                    }
+					String qName = startElement.getName().getLocalPart();
+
+					//If the start element is the root element of the fragement instantiate the StringBuilder to build the fragment and handle the start element.
+					if (qName.equals(rootElementLocalName)) {
+						nextElementBuilder = new StringBuilder();
+						handleStartElement(startElement, nextElementBuilder);
+					} else {
+						handleStartElement(startElement, nextElementBuilder);
+					}
 				}
-				
-				if(event.getEventType() == XMLStreamConstants.END_ELEMENT){
+
+				if (event.getEventType() == XMLStreamConstants.END_ELEMENT) {
 					EndElement endElement = event.asEndElement();
 					handleEndElement(endElement, nextElementBuilder);
-					
-					if(endElement.getName().getLocalPart().equals(rootElementLocalName)){
+
+					//If the end element is the root element of the fragment, then return the fragement string
+					if (endElement.getName().getLocalPart().equals(rootElementLocalName)) {
 						return nextElementBuilder.toString();
 					}
 				}
-				
-				if(event.getEventType() == XMLStreamConstants.CHARACTERS){
+
+				if (event.getEventType() == XMLStreamConstants.CHARACTERS) {
 					Characters characters = event.asCharacters();
 					handleCharacters(characters, nextElementBuilder);
 				}
-				
-				
+
 			}
-		
+
 		} catch (XMLStreamException e) {
-			
+
 			e.printStackTrace();
-			throw new RuntimeException(e.getMessage(),e);
+			throw new RuntimeException(e.getMessage(), e);
 		}
-		
+
 		return null;
 	}
 
 	private void handleCharacters(Characters characters, StringBuilder nextElementBuilder) {
-		if(nextElementBuilder == null){
+		if (nextElementBuilder == null) {
 			return;
 		}
 		
-		nextElementBuilder.append(characters.getData());
-		
+		//Ignore any new line characters
+		if(characters.getData().equals("\n\t") || characters.getData().equals("\n") || characters.getData().equals("\t")){
+			return;
+		}
+
+		//Trim the leading or trailing spaces
+		nextElementBuilder.append(characters.getData().trim());
+
 	}
 
 	private void handleEndElement(EndElement endElement, StringBuilder nextElementBuilder) {
-		
-		if(nextElementBuilder == null){
+
+		if (nextElementBuilder == null) {
 			return;
 		}
-		
+
 		nextElementBuilder.append(START_TAG).append("/").append(endElement.getName().getLocalPart()).append(END_TAG);
 	}
 
 	private void handleStartElement(StartElement startElement, StringBuilder nextElementBuilder) {
-		
-		if(nextElementBuilder == null){
+
+		if (nextElementBuilder == null) {
 			return;
 		}
-		
+
 		nextElementBuilder.append(START_TAG).append(startElement.getName().getLocalPart());
-		
+
 		Iterator<Attribute> attributes = startElement.getAttributes();
-		
-		while(attributes.hasNext()){
+
+		while (attributes.hasNext()) {
 			Attribute attribute = attributes.next();
 			String attrName = attribute.getName().getLocalPart();
 			String attrValue = attribute.getValue();
-			
+
 			nextElementBuilder.append(" ").append(attrName).append("='").append(attrValue).append("'");
 		}
-		
+
 		nextElementBuilder.append(END_TAG);
-		
+
 	}
-	
-	
 
 }
